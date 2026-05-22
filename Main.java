4b30c99c067c08df19eef5aeb1813d20f0018049
harishva247 <727723euec058@skcet.ac.in>
2026-05-22 09:33:56 +0530

    added function

--- Main.java
+++ Main.java
@@ -1,7 +1,44 @@
 import java.util.*;
+public class Task{
+    String task;
+    boolean completed;
+    void Task(String t){
+        task=t;
+        completed=false;
+    }
+}
 public class Main{
+    static Scanner sc=new Scanner(System.in);
+    public static void addTask(ArrayList<String>tasks){
+        System.out.print("Enter the task: ");
+        String t=sc.nextLine();
+        tasks.add(t);
+    }
+    public static void deleteTask(ArrayList<String>tasks){
+        System.out.print("Enter the completed the task number : ");
+        int index=sc.nextInt();
+        sc.nextLine();
+        if(index<1 || index>tasks.size()){
+            System.out.println("Enter an valid task number.");
+            deleteTask(tasks);
+        }
+        else{
+            tasks.remove(index-1);
+            viewTask(tasks);
+        }
+    }
+    public static void viewTask(ArrayList<String>tasks){
+        if(tasks.isEmpty()){
+            System.out.println("The list is Empty.");
+        }
+        else{
+            System.out.println("Tasks: ");
+            for(int i=0;i<tasks.size();i++){
+                System.out.println((i+1)+"."+tasks.get(i));
+            }
+        }
+    }
     public static void main(String[] args){
-        Scanner sc=new Scanner(System.in);
         ArrayList<String> tasks=new ArrayList<>();
         while(true){
             System.out.println("choice\n"+
@@ -12,28 +49,21 @@ public class Main{
             int choice=sc.nextInt();
             sc.nextLine();
             if(choice==1){
-                System.out.println("Enter the task");
-                String t=sc.nextLine();
-                tasks.add(t);
+                addTask(tasks);
             }
             else if(choice==2){
-                System.out.println("Enter the completed the task number");
-                int index=sc.nextInt();
-                tasks.remove(index-1);
+                deleteTask(tasks);
             }
             else if(choice==3){
-                if(tasks.isEmpty()){
-                    System.out.println("The list is Empty.");
-                }
-                else{
-                    System.out.println("Tasks: ");
-                    for(int i=0;i<tasks.size();i++){
-                        System.out.println((i+1)+"."+tasks.get(i));
-                    }
-                }
+                viewTask(tasks);
+            }
+            else if (choice==4){
+                System.out.println("Exiting..............");
+                System.out.flush();
+                break;
             }
             else{
-                break;
+                System.out.println("Enter an valid choice");
             }
         }
         sc.close();
